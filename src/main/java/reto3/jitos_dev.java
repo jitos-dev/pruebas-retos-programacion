19eea0c3908514567236423382d1f64beb08cd3d
Juan José García <jitos86@gmail.com>
2023-01-17 07:55:15 +0100

    Initial commit Reto3 Generador de contraseñas

---
+++ src/main/java/reto3/jitos_dev.java
@@ -0,0 +1,28 @@
+package reto3;
+
+/*
+ * Reto #3: EL GENERADOR DE CONTRASEÑAS
+ * Dificultad: Media | Publicación: 16/01/23 | Corrección: 23/01/23
+ *
+ * Enunciado
+ * Escribe un programa que sea capaz de generar contraseñas de forma aleatoria.
+ * Podrás configurar generar contraseñas con los siguientes parámetros:
+ * - Longitud: Entre 8 y 16.
+ * - Con o sin letras mayúsculas.
+ * - Con o sin números.
+ * - Con o sin símbolos.
+ * (Pudiendo combinar todos estos parámetros entre ellos)
+ */
+
+public class jitos_dev {
+
+    public static void main(String[] args) {
+
+    }
+
+    public String generatePassword(int length, boolean canUppercase, boolean canNumbers, boolean canSymbols) {
+
+
+        return null;
+    }
+}
